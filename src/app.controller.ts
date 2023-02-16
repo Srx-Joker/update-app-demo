f55ca57056a836bec782163ebc33d1dfcdd06883
吕翔 <2913538760@qq.com>
2023-02-17 02:02:53 +0800

    完成更新机制

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,65 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Res,Post, Body, Query ,UploadedFile } from '@nestjs/common';
+import { Response } from 'express';
+import { FormDataRequest } from 'nestjs-form-data/dist/decorators';
 import { AppService } from './app.service';
+import { UpdateService } from './update.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  private updateService: UpdateService;
+
+  constructor(
+    private readonly appService: AppService
+    
+  ) {
+    this.updateService = new UpdateService();
+  }
 
   @Get()
   getHello(): string {
     return this.appService.getHello();
   }
+
+  @Get('checkUpdate')
+  updateApp(@Query("version")version:string): string {
+    if(!version){
+      console.log("版本号为空")
+      return
+    }
+
+    console.log("当前版本：",version)
+    return this.updateService.CheckUpdate(version).toString();
+  }
+  
+  @Get('download/:apkName')
+  // 下载文件接口
+  downloadApp(@Query("userID") id:string,@Res() res: Response) {
+    if(!id){
+      console.log("用户ID为空")
+      return
+    }
+    this.updateService.start(false,{id,res});
+  }
+
+  @Post('upload/')
+  @FormDataRequest()
+  uploadApp(@Body("file") file:Buffer,@Body("version")version:string) {
+    
+    if(!file){
+      console.log("上传文件为空")
+      return
+    }
+
+    if(!version){
+      console.log("上传版本为空")
+      return
+    }
+
+
+    const buffer = Buffer.from(file.buffer);
+
+    this.updateService.upload(buffer,version);
+    
+  }
+
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
+import { NestjsFormDataModule } from 'nestjs-form-data';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    NestjsFormDataModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
