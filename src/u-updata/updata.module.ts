41055e6fdf5caa3692a90857c764adb00fdad904
吕翔 <2913538760@qq.com>
2023-02-20 16:26:24 +0800

    添加lib文件

---
+++ src/u-updata/updata.module.ts
@@ -0,0 +1,16 @@
+import { Module } from '@nestjs/common';
+import { NestjsFormDataModule } from 'nestjs-form-data';
+
+
+@Module({
+  imports: [
+
+  ]
+})
+export class UpdataModule {
+    constructor() {
+        return {
+
+        };
+    }
+}
